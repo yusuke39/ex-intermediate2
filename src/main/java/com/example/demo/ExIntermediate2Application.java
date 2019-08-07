e6b28f7c7f08eee2f87b50c128de90386a28e945
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-07 16:45:32 +0900

    最初のファイルをコミット firstCommit

---
+++ src/main/java/com/example/demo/ExIntermediate2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExIntermediate2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExIntermediate2Application.class, args);
+	}
+
+}
